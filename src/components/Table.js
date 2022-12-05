db258519b8f030796d4d8c718ae8cd14c851ae87
Renato <renato92.hora@gmail.com>
2022-12-05 18:28:23 -0300

    Requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,6 +6,10 @@ export default function Table() {
   const [tableHeads, setTableHeads] = useState([]);
   const [find, setFind] = useState('');
   const [filters, setFilters] = useState([]);
+  const [colunas, setColunas] = useState('population');
+  const [handle, setHandle] = useState('maior que');
+  const [valor, setValor] = useState(0);
+  const [numeroFiltro, setNumeroFiltro] = useState(null);
 
   useEffect(() => {
     const fetching = async () => {
@@ -27,6 +31,24 @@ export default function Table() {
     return null;
   });
 
+  const handleConditioning = (element) => {
+    switch (element) {
+    case 'maior que':
+      setNumeroFiltro(planetsFilter
+        .filter((elemento) => (+elemento[colunas] > +valor)));
+      break;
+    case 'menor que':
+      setNumeroFiltro(planetsFilter
+        .filter((elemento) => (+elemento[colunas] < +valor)));
+      break;
+    case 'igual a':
+      setNumeroFiltro(planetsFilter
+        .filter((elemento) => (+elemento[colunas] === +valor)));
+      break;
+    default:
+    }
+  };
+
   return (
     <div>
       <div>
@@ -43,6 +65,39 @@ export default function Table() {
           />
         </label>
       </div>
+      <select
+        data-testid="column-filter"
+        value={ colunas }
+        onChange={ ({ target: { value } }) => setColunas(value) }
+      >
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ handle }
+        onChange={ ({ target: { value } }) => setHandle(value) }
+      >
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a</option>
+      </select>
+      <input
+        data-testid="value-filter"
+        type="number"
+        value={ valor }
+        onChange={ ({ target: { value } }) => setValor(value) }
+      />
+      <button
+        data-testid="button-filter"
+        type="button"
+        onClick={ () => handleConditioning(handle) }
+      >
+        Filtrar
+      </button>
       <table>
         <thead>
           <tr>
@@ -52,7 +107,7 @@ export default function Table() {
           </tr>
         </thead>
         <tbody>
-          {planetsFilter.map((planet) => (
+          {/* {planetsFilter.map((planet) => (
             <tr key={ planet.name }>
               <td>{planet.name}</td>
               <td>{planet.rotation_period}</td>
@@ -67,6 +122,41 @@ export default function Table() {
               <td>{planet.created}</td>
               <td>{planet.edited}</td>
               <td>{planet.url}</td>
+            </tr>
+          ))} */}
+          {planetsFilter.length > 0 && numeroFiltro === null
+           && planetsFilter.map((planet) => (
+             <tr key={ planet.name }>
+               <td>{planet.name}</td>
+               <td>{planet.rotation_period}</td>
+               <td>{planet.orbital_period}</td>
+               <td>{planet.diameter}</td>
+               <td>{planet.climate}</td>
+               <td>{planet.gravity}</td>
+               <td>{planet.terrain}</td>
+               <td>{planet.surface_water}</td>
+               <td>{planet.population}</td>
+               <td>{planet.films}</td>
+               <td>{planet.created}</td>
+               <td>{planet.edited}</td>
+               <td>{planet.url}</td>
+             </tr>
+           ))}
+          {numeroFiltro !== null && numeroFiltro.map((planetInfo) => (
+            <tr key={ planetInfo.name }>
+              <td>{planetInfo.name}</td>
+              <td>{planetInfo.rotation_period}</td>
+              <td>{planetInfo.orbital_period}</td>
+              <td>{planetInfo.diameter}</td>
+              <td>{planetInfo.climate}</td>
+              <td>{planetInfo.gravity}</td>
+              <td>{planetInfo.terrain}</td>
+              <td>{planetInfo.surface_water}</td>
+              <td>{planetInfo.population}</td>
+              <td>{planetInfo.films}</td>
+              <td>{planetInfo.created}</td>
+              <td>{planetInfo.edited}</td>
+              <td>{planetInfo.url}</td>
             </tr>
           ))}
         </tbody>
